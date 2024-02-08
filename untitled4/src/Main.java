4b616caa9693b76e07b747af2fa71d6dd9cb67cc
M0rik <159443850+M0rik@users.noreply.github.com>
2024-02-08 16:41:39 +0300

    Add files via upload

---
+++ untitled4/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Main {
+/*   public static void main(String[] args) {
+        int [] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0){
+                arr[i] = 1;
+            } else if (arr[i]==1) {
+                arr[i] = 0;
+
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+*/
+/*        public static void main(String[] args) {
+          int[] arr = new int[100];
+
+            for (int i = 1; i < arr.length; i++) {
+                System.out.print((arr[i] = i)+ " ");
+            }
+        }
+
+
+*/
+/*    public static void main(String[] args) {
+        int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6){
+                arr[i] *= 2;
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+ */
+    
+}
